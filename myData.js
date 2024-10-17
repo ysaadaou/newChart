b27f84da167d9d61e5bfb54018d0965c8272d39f
ysaadou <yhiasaad748@gmail.com>
2024-10-17 06:47:08 +0100

    mme

---
+++ myData.js
@@ -0,0 +1,818 @@
+const today = new Date();
+
+// Extract the year, month, and day
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+const day = String(today.getDate()).padStart(2, "0");
+
+// Format the date as yyyy-mm-dd
+const now = `${year}-${month}-${day}`;
+
+const gantData = {
+  4500012490: {
+    flag: "./french.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-12",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-07-12",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-22",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-01-08",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-01-08",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "ETA Date Supplier JFC1",
+          startingDate: "2025-03-12",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "ETA Date Supplier JFC2",
+          startingDate: "2025-01-31",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "ETA Date Supplier JFC4",
+          startingDate: "2025-01-31",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013501: {
+    flag: "./spain.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-19",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-07-19",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-04-18",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "ETA Date Supplier JFC1",
+          startingDate: "2025-04-18",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-14",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "ETA Date Supplier JFC2",
+          startingDate: "2025-02-18",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-01-30",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "ETA Date Supplier JFC4",
+          startingDate: "2025-01-30",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  4500012607: {
+    flag: "./french.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-03",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-07-03",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2024-12-04",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Eta Date supplier JFC1",
+          startingDate: "2025-03-03",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  "4500012656 / 4500012657": {
+    flag: "./morroco.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-08",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-08-24",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-01-08",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-02-23",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  4500012610: {
+    flag: "./turkey.jpg",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-04",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-08-28",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-26",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-02-26",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-26",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC2",
+          startingDate: "2025-02-26",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-26",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2025-02-26",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013344: {
+    flag: "./india.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-04",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-08-16",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-03-27",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Eta Date Supplier JFC1",
+          startingDate: "2025-03-27",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-03-27",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta Date Supplier JFC2",
+          startingDate: "2025-02-14",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-03-27",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta Date Supplier JFC4",
+          startingDate: "2025-01-31",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013003: {
+    flag: "./morroco.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-29",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-07-29",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-06-09",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta supplier JFC1",
+          startingDate: "2025-05-26",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-06-09",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta supplier JFC2",
+          startingDate: "2025-05-26",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-06-25",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta supplier JFC4",
+          startingDate: "2025-05-26",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013060: {
+    flag: "./french.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-29",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-07-29",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-06-02",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Eta supplier JFC1",
+          startingDate: "2025-05-19",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-06-02",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta supplier JFC2",
+          startingDate: "2025-05-19",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-06-09",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta supplier JFC4",
+          startingDate: "2025-05-19",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013028: {
+    flag: "./turkey.jpg",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-08-13",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-08-26",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-22",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-03-03",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-22",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC2",
+          startingDate: "2025-03-03",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-06-02",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2025-03-03",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  45000126717: {
+    flag: "./morroco.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-07-04",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-07-04",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-01",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-02-01",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  4500013350: {
+    flag: "./china.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-08-16",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-08-16",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2024-11-14",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2024-11-15",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  4500013347: {
+    flag: "./china.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-08-16",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-09-12",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-01-15",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-02-25",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-01-15",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC2",
+          startingDate: "2025-01-15",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2024-11-15",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2025-01-15",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  "4500007152-53/J-2023": {
+    flag: "./morroco.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-05-09",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-08-24",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2024-10-10",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+
+        {
+          name: "Eta date supplier JFC2",
+          startingDate: "2024-10-30",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-01-02",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2024-10-30",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+      ],
+    },
+  },
+  "4500013694/JESA/24": {
+    flag: "./morroco.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-12",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-09-12",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2024-12-11",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-01-06",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2024-12-11",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC2",
+          startingDate: "2024-12-15",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2024-12-11",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2024-12-06",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  "4500013524/24": {
+    flag: "./bahrein.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-10",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-09-27",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-07",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-02-07",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-07",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC2",
+          startingDate: "2025-02-07",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-07",
+          endingDate: now,
+          status: "Annexes not yet signed",
+        },
+
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2025-02-07",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+  "4500013627/24": {
+    flag: "./emirates.png",
+    gantData: {
+      milestones: [
+        {
+          name: "PO signature",
+          startingDate: "2024-09-20",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Annexes signature",
+          startingDate: "2024-09-26",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC1",
+          startingDate: "2025-02-20",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC1",
+          startingDate: "2025-02-17",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC2",
+          startingDate: "2025-02-20",
+          endingDate: now,
+          status: "",
+        },
+
+        {
+          name: "Eta date supplier JFC2",
+          startingDate: "2025-02-17",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "PO CONTRACTUAL DELIVERY DATE JFC4",
+          startingDate: "2025-02-07",
+          endingDate: now,
+          status: "",
+        },
+        {
+          name: "Eta date supplier JFC4",
+          startingDate: "2025-02-17",
+          endingDate: now,
+          status: "",
+        },
+      ],
+    },
+  },
+};
+
+export default gantData;
---
+++ otherData.js
@@ -0,0 +1,99 @@
+const otherData = {
+  4500012490: {
+    otherData: [
+      {
+        supplier: "SAACKE INDUSTRIAL BURNERS",
+        productName: "COMBUSTION CHAMBERS",
+      },
+    ],
+  },
+  4500013501: {
+    otherData: [
+      {
+        supplier: "MICROMOL GRINDING EXPERIENCE",
+        productName: "PENDULAR MILLS  ",
+      },
+    ],
+  },
+  4500012607: {
+    otherData: [{ supplier: "MIXEL", productName: "AGITATOURS " }],
+  },
+  "4500012656 / 4500012657": {
+    otherData: [
+      {
+        supplier: "SOMACHAME & MENASTEEL Industry SARL ",
+        productName: "Reactors and Mixers",
+      },
+    ],
+  },
+  4500012610: {
+    otherData: [{ supplier: "BSK Global", productName: "Conveyors " }],
+  },
+  4500013344: {
+    otherData: [
+      {
+        supplier: "XOMOX",
+        productName: "Control Plug valves and Plug ON /OFF valves",
+      },
+    ],
+  },
+  4500013003: {
+    otherData: [
+      { supplier: "SOFIMED", productName: "Dryer prescrubber pumps" },
+    ],
+  },
+  4500013060: {
+    otherData: [
+      { supplier: "SOFIMED", productName: "Caustic unloading pumps" },
+    ],
+  },
+  4500013028: {
+    otherData: [
+      { supplier: "Milton ROY", productName: "Caustic metering pumps" },
+    ],
+  },
+  45000126717: { otherData: [{ supplier: "KGM", productName: "Diverters" }] },
+  4500013350: {
+    otherData: [{ supplier: "SCIF", productName: "Caustic tank" }],
+  },
+  4500013347: {
+    otherData: [
+      { supplier: "VALVTEC", productName: "Butterfly on off valves" },
+    ],
+  },
+  "4500007152-53/J-2023": {
+    otherData: [
+      { supplier: "SOMACHAME MENASTEEL ", productName: "MIXERS & REACTORS " },
+    ],
+  },
+  "4500013694/JESA/24": {
+    otherData: [
+      {
+        supplier: "SMART COMPOSIT",
+        productName: " Dryer Pre-scrubbers & Duct",
+      },
+    ],
+  },
+  4500013749: {
+    otherData: [{ supplier: "KROHNE", productName: "Magnetic Flow meters" }],
+  },
+  4500013478: {
+    otherData: [{ supplier: "ROBINSON FANS", productName: "Fans" }],
+  },
+  "4500013524/24": {
+    otherData: [{ supplier: "YOKOGAWA", productName: "DCS " }],
+  },
+  "4500013627/24": {
+    otherData: [
+      { supplier: "NETENERGY", productName: "Control Ball and Globe Valves " },
+    ],
+  },
+  4500013596: {
+    otherData: [
+      { supplier: "Endress & Houser", productName: "Nuclear Density Meter" },
+    ],
+  },
+};
+
+export default otherData;
+
